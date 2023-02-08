aaeecc5678575591bfdb0a1199dac8e7b81ab2ba
another1124 <another10508@gmail.com>
2023-02-08 19:37:41 +0800

    add comment

--- Myview.py
+++ Myview.py
@@ -1,5 +1,5 @@
 import json
-from discord.ext import commands
+from discord.ext import commands,tasks
 from discord.ui import Button , View, Select ,TextInput,Modal
 from discord import app_commands
 import discord
@@ -8,23 +8,31 @@ import random
 
 
 embed = discord.Embed()
-leavelist=[]
+leavelist=[] ## 假單列表
+
+def get_leve_list():## 回傳假單列表
+    return leavelist
 
 ###假單
 class leavemodal(Modal,title = 'leave form'):
     def __init__(self):
         super().__init__()
-        self.value = None
     
-    year=TextInput(label='year',style=discord.TextStyle.long,placeholder='title',default=datetime.datetime.now().year,required=True,max_length=4,row=0)
-    month=TextInput(label='month',style=discord.TextStyle.long,placeholder='title',default=datetime.datetime.now().month,required=True,max_length=2,row=0)
-    day=TextInput(label='day',style=discord.TextStyle.long,placeholder='title',default=datetime.datetime.now().day,required=True,max_length=2,row=0)
-    cause=TextInput(label='cause',style=discord.TextStyle.long,placeholder='title',default='None',required=True,max_length=50,row=1)
+    year=TextInput(label='year',style=discord.TextStyle.short,placeholder='title',default=datetime.datetime.now().year,required=True,max_length=4,row=0)
+    month=TextInput(label='month',style=discord.TextStyle.short,placeholder='title',default=datetime.datetime.now().month,required=True,max_length=2,row=1)
+    day=TextInput(label='day',style=discord.TextStyle.short,placeholder='title',default=datetime.datetime.now().day,required=True,max_length=2,row=2)
+    reason=TextInput(label='reason',style=discord.TextStyle.long,placeholder='title',default='None',required=True,max_length=50,row=3)
 
+    ###送出假單
     async def on_submit(self, interaction: discord.Interaction):
-        leavelist.append({'user':str(interaction.user),'cause':self.cause,'time':datetime.datetime.strptime(self.year+self.month+self.day,"%Y%m%d")})
-        print(leavelist)
-        await interaction.response.send_message("已經送出假單了",ephemeral=True)
+        try:
+            leavelist.append({'user':str(interaction.user),
+                              'reason':str(self.reason),
+                              'date':datetime.datetime.strptime(str(self.year)+str(self.month)+str(self.day),"%Y%m%d").strftime("%Y-%m-%d")})
+            print(leavelist)
+            await interaction.response.send_message("已經送出假單了",ephemeral=True)
+        except:
+            await interaction.response.send_message("輸入格式錯誤",ephemeral=True)
     
 
 ###選單
@@ -32,37 +40,44 @@ class menu(View):
     def __init__(self):
         super().__init__()
         self.value = None
-        
+    
+    ##擲骰子
     @discord.ui.button(label="roll dice",style=discord.ButtonStyle.red)
     async def roll(self,interaction, button):
         v=rolldice()
         
         await interaction.response.send_message(view=v)
+    ##錄音(沒有用)
     @discord.ui.button(label="record",style=discord.ButtonStyle.red)
     async def record(self,interaction, button ):
         v=record()
         
         await interaction.response.send_message(view=v)
+    ##創立投票
     @discord.ui.button(label="create vote",style=discord.ButtonStyle.red)
     async def vote(self,interaction, button ):
         await interaction.response.send_modal(votemodal())
+
 
 ### 投票介面
 class voteui(View):
     
     def __init__(self,embed,l1,l2,l3):
         super().__init__()
-        self.c1=0
-        self.c2=0
-        self.c3=0
-        self.l1=l1
-        self.l2=l2
-        self.l3=l3
+        self.c1=0  ###choice1的票數
+        self.c2=0  ###choice2的票數
+        self.c3=0  ###choice3的票數
+
+        self.l1=l1 ###choice1的名稱
+        self.l2=l2 ###choice2的名稱
+        self.l3=l3 ###choice3的名稱
         self.votelist=[] ## 記錄誰投票了
-        self.embed=embed
+        self.embed=embed ## 投票的embed
     
     def update(self): ##更新票數
         return self.l1+"  "+str(self.c1)+'\n'+self.l2+"  "+str(self.c2)+'\n'+self.l3+"  "+str(self.c3)
+
+    
     @discord.ui.button(label="choice1",style=discord.ButtonStyle.green)
     async def choice1(self,interaction, button ):
         if interaction.user.id not in self.votelist:
@@ -72,6 +87,7 @@ class voteui(View):
             await interaction.response.edit_message(embed=self.embed)
         else:
             await interaction.response.send_message("你已經投過票了",ephemeral=True)
+
     @discord.ui.button(label="choice2",style=discord.ButtonStyle.green)
     async def choice2(self,interaction, button ):
         if interaction.user.id not in self.votelist:
@@ -81,6 +97,7 @@ class voteui(View):
             await interaction.response.edit_message(embed=self.embed)
         else:
             await interaction.response.send_message("你已經投過票了",ephemeral=True)
+
     @discord.ui.button(label="choice3",style=discord.ButtonStyle.green)
     async def choice3(self,interaction, button ):
         if interaction.user.id not in self.votelist:
@@ -91,7 +108,7 @@ class voteui(View):
         else:
             await interaction.response.send_message("你已經投過票了",ephemeral=True)
     
-##投票設定
+##投票設定表單
 class votemodal(Modal,title = 'vote setting'):
     t=TextInput(label='title',style=discord.TextStyle.short,placeholder='title',default='None',required=True,max_length=50,row=0)
     s1=TextInput(label='choice 1',style=discord.TextStyle.short,placeholder='choice 1',default='None',required=True,max_length=50,row=1)
@@ -106,20 +123,21 @@ class votemodal(Modal,title = 'vote setting'):
         
         embed = discord.Embed(title=str(self.t),description=l1+'  0\n'+l2+'  0\n'+l3+'  0',
                               timestamp=datetime.datetime.now(),color=discord.Color.red())
-       # embed.set_author(name= interaction.user, icon_url=interaction.user.avatar)  #設定誰能看到
+        embed.set_author(name= interaction.user, icon_url=interaction.user.avatar)  #設定誰發起的
         v=voteui(embed=embed,l1=l1,l2=l2,l3=l3)
         await interaction.response.send_message(view=v,embed=embed)
 
 
-##丟骰子
+##丟骰子的介面
 class rolldice(View):
     def __init__(self):
         super().__init__()
-        self.value = None
 
     @discord.ui.button(label="roll",style=discord.ButtonStyle.red)
     async def roll(self,interaction, button):
         i=random.randint(1,6)
+
+        ##圖片自己找的 可修改
         if i==1:
             embed.set_thumbnail(url="https://media.discordapp.net/attachments/774950219669307397/1069858458163228702/1.png?width=467&height=467")
         if i==2:
@@ -134,11 +152,35 @@ class rolldice(View):
             embed.set_thumbnail(url="https://media.discordapp.net/attachments/774950219669307397/1069858459333439498/6.png?width=467&height=467")
         await interaction.response.edit_message(embed=embed)
 
+## 定義個人 Client
+class MyClient(discord.Client):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+    
+    ##定時工作請假提醒
+    @tasks.loop(hours=19)
+    async def checkleave(self): 
+        channellist=self.get_all_channels()
+        print(channellist)
+        channel=self.get_channel(1072052767587303494)
+        today=datetime.datetime.now().strftime("%Y-%m-%d")
+        if(leavelist==[]):
+            return
+        else:
+            for i in leavelist:
+                if i['date'] == today:
+                    await channel.send(f"請假告示\n 姓名:{i['user']} \n日期:{i['date']} \n原因:{i['reason']}\n\n")
+
+
+        
+    
+### 錄音功能(暫時沒有用)
 class record(View):
     def __init__(self):
         super().__init__()
         self.value = None
-        
+    
+
     @discord.ui.button(label="start",style=discord.ButtonStyle.red)
     async def start(self,interaction, button):
         channel = interaction.channel
--- index.py
+++ index.py
@@ -2,103 +2,138 @@ import pandas as pd
 import pathlib
 import json
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
 from discord.ui import Button , View, Select
 from discord import app_commands
 import threading
 import Myview
-import requests
 import datetime
 import nacl
 
+####找到json檔案的路徑
 path = pathlib.Path(__file__).parent.absolute()
-####test
 
+
+##讀取環境變數
 with open(str(path)+'/env.json') as f:
     appData = json.load(f)
+
+
 
 CLIENT_ID = appData['CLIENT_ID']
 GUILD_ID = appData['GUILD_ID']
 TOKEN=appData['TOKEN']
 
+
+###本來用來做錄音 但是沒有成功 現在沒有用
 re_list=[]
+###
+
+##embed 物件初始化
 embed = discord.Embed()
+
+## 設定client intents
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+
+## 初始化改寫過的client  Myclient 繼承 discord.Client   ( type(Myclient) = type(discord.Client) )
+client = Myview.MyClient(intents=intents)
+
+## 建立指令樹 用來存放 slash command
 tree = app_commands.CommandTree(client)
+
+## 初始化bot (目前尚未有實際做用)  bot 主要用來控制 bot帳號本身的行為 (邀請加入語音頻道等等,發送訊息 大部分的功能都可以被client取代)
 bot = commands.Bot(intents=intents,command_prefix='!')
 
 
 
-#####slash command
+#####slash command 
+
+##測試用 輸入/ping 回復 pong
 @tree.command(name="ping",description='Replies with Pong',guild=discord.Object(id=GUILD_ID))
 async def ping(interaction):
     await interaction.response.send_message("Pong")
+
+##測試用 輸入/hello 回復 hello
 @tree.command(name="hello",description='Replies with hello',guild=discord.Object(id=GUILD_ID))
 async def hello(interaction):
-    await interaction.response.send_message("hello")/ㄥ
+    await interaction.response.send_message("hello")
+
+##執行請假流程 輸入/leave 回傳請假表單
 @tree.command(name="leave",description="call leave form",guild=discord.Object(id=GUILD_ID))
-async def leave(interaction):
+async def askleave(interaction):
     await interaction.response.send_modal(Myview.leavemodal())
+
+##查詢當日請假人員 輸入/checkleave 回傳當日請假人員
+@tree.command(name="checkleave",description="check leave form",guild=discord.Object(id=GUILD_ID))
+async def checkleave(interaction):
+    leavelist=Myview.get_leve_list()
+    today=datetime.datetime.now().strftime("%Y-%m-%d")
+    for i in leavelist:
+        if i['date'] == today:
+            await interaction.response.send_message(f"姓名:{i['user']} \n日期:{i['date']} \n原因:{i['reason']}\n\n")
+
+##呼叫功能選單 /menu 回傳功能選單
 @tree.command(name="menu",description='call menu',guild=discord.Object(id=GUILD_ID))
 async def interface(interaction):   
     view = Myview.menu()
     
-    await interaction.response.send_message(view=view,ephemeral=True) ### ephmeral =只有自己看得到
-    
-#########
+    await interaction.response.send_message(view=view,ephemeral=True) ### ephmeral = true 只有自己看得到
 
-####bot 
+
+
+
+
+
+####bot (目前沒有實際用途)
+
+##邀請機器人加入語音頻道
 @bot.command(pass_context=True)
-async def invite(ctx):
-        
-        channel = ctx.author.voice.channel
-        voice =await channel.connect()
-       
-        print(type(voice))
-        re_list.append(voice)
-        return
-  
+async def invite(ctx):     
+    channel = ctx.author.voice.channel
+    voice =await channel.connect()
+    print(type(voice))
+    re_list.append(voice)
+    return
+
+##機器人離開語音頻道
 @bot.command(pass_context=True)
 async def leave(ctx):
-
-        await ctx.voice_client.disconnect()
-        re_list.pop()
-
-
+    await ctx.voice_client.disconnect()
+    re_list.pop()
 
 #########
 
 
-####client 
+####event 
 
-
+##client 啟動完成
 @client.event
 async def on_ready():
+    client.checkleave.start()## 執行定時任務
     await tree.sync(guild=discord.Object(id=GUILD_ID))  ### 部屬 slash command
 
+## 頻道內有人發送訊息
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     pass
-
 #########
-
 
 
 def client_run():
     client.run(TOKEN)
+
+
 def bot_run():
     bot.run(TOKEN)
 
-t1=threading.Thread(target=client_run)
-#t2=threading.Thread(target=bot_run)
+t1=threading.Thread(target=client_run)  ### client thread
+#t2=threading.Thread(target=bot_run)    ### bot thread
 
 t1.start()
 #t2.start()
-
 
 t1.join()
 #t2.join()

6795ed7e12b07b11e35ed83c3b3ddd81e9365d31
Slope <60516263+Slope86@users.noreply.github.com>
2023-02-10 20:21:49 +0800

    [add] DataBase context manager

--- api/database.py
+++ api/database.py
@@ -1,11 +1,19 @@
 import sqlite3
 
-conn=sqlite3.connect("test.db")
 
+class dbopen(object):
+    """
+    Simple CM for sqlite3 databases. Commits everything at exit.
+    """
 
-def databasecommand(sql):
-    conn.execute(sql)
-    
+    def __init__(self, path):
+        self.path = path
 
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.path)
+        self.cursor = self.conn.cursor()
+        return self.cursor
 
-
+    def __exit__(self, exc_class, exc, traceback):
+        self.conn.commit()
+        self.conn.close()
